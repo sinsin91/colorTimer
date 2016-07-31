d9dd3dc9a86d464dd78942e17306d4a72a52e883
Yoon Sin <sin@exabytes.com>
2016-08-01 02:19:57 +0800

    Upload full source.

---
+++ content.js
@@ -0,0 +1,21 @@
+setInterval(function(){ 
+
+var now = new Date();
+
+var hour = now.getHours();
+var minute = now.getMinutes();
+var second = now.getSeconds();
+
+hour = ('0' + hour).slice(-2);
+minute = ('0' + minute).slice(-2);
+second = ('0' + second).slice(-2);
+
+var code = '#'+hour+minute+second;
+
+document.getElementById("code").innerHTML = code;
+
+
+}, 1000);
+
+
+
